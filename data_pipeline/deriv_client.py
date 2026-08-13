d4172e63151a523adf8d7a73f1de9fd033b79ce3
michael-ogundipe <ogundipeibukun51@gmail.com>
2026-08-13 12:20:21 +0100

    feat: stream live ticks from Deriv

--- data_pipeline/deriv_client.py
+++ data_pipeline/deriv_client.py
@@ -3,7 +3,9 @@ import json
 
 import websockets
 
-from models import Timeframe
+from models import Candle, Timeframe
+from parser import parse_candles, parse_tick
+from parser import parse_tick
 
 
 DERIV_WS_URL = "wss://api.derivws.com/trading/v1/options/ws/public"
@@ -17,12 +19,13 @@ class DerivClient:
         self.websocket = await websockets.connect(DERIV_WS_URL)
         print("Connected to Deriv!")
 
+    
     async def get_candles(
         self,
         symbol: str,
         timeframe: Timeframe,
         count: int,
-    ):
+    ) -> list[Candle]:
         request = {
             "ticks_history": symbol,
             "end": "latest",
@@ -37,7 +40,28 @@ class DerivClient:
 
         data = json.loads(message)
 
-        return data
+        return parse_candles(
+            data,
+            symbol,
+            timeframe,
+        )
+
+    async def subscribe_ticks(self, symbol: str):
+            request = {
+                "ticks": symbol,
+                "subscribe": 1,
+            }
+    
+            await self.websocket.send(json.dumps(request))
+    
+            while True:
+                message = await self.websocket.recv()
+                data = json.loads(message)
+    
+                if data.get("msg_type") != "tick":
+                    continue
+    
+                yield parse_tick(data)
 
 
 async def main():
@@ -45,14 +69,19 @@ async def main():
 
     await client.connect()
 
-    candles = await client.get_candles(
-        symbol="R_75",
-        timeframe=Timeframe.H4,
-        count=10,
-    )
+    # candles = await client.get_candles(
+    #     symbol="R_75",
+    #     timeframe=Timeframe.H4,
+    #     count=10,
+    # )
 
-    print(json.dumps(candles, indent=2))
+    # for candle in candles:
+    #     print(candle)
+
+    async for tick in client.subscribe_ticks("R_75"):
+        print(tick)
 
 
 if __name__ == "__main__":
     asyncio.run(main())
+
